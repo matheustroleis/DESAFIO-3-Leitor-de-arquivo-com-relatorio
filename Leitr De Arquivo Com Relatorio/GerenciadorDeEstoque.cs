b26bb84aa2c7fd2930c32b86eca533b95207a6b1
matheus.alves <matheustorleis1981@gmail.com>
2025-08-25 12:18:19 -0300

    Adicionar Produto e Listar produto, além do teste das excption

--- Leitr De Arquivo Com Relatorio/GerenciadorDeEstoque.cs
+++ Leitr De Arquivo Com Relatorio/GerenciadorDeEstoque.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +14,7 @@ internal class GerenciadorDeEstoque
 
     public void ImportarArquivo(string caminhoDoArquivo)
     {
-        var linhas = File.ReadAllLines(caminhoDoArquivo);//Lê todas as linhas do arquivo na váriavel linha 
+        var linhas = File.ReadAllLines(caminhoDoArquivo);//Lê todas as linhas do arquivo na váriavel linhas 
         for (int i = 1; i < linhas.Length; i++) // i = 1 -> Pula o cabeçalho
         {
             var linha = linhas[i]; // toda vez que passa no loop pega a linha atual
@@ -21,7 +22,7 @@ internal class GerenciadorDeEstoque
             {
                 var colunas = linha.Split(','); //array de string -> [Bolsa Transversal,119.5,Acessorios]
 
-                if (colunas.Length != 3)
+                if (colunas.Length != 4)
                 {
                     throw new FormatoInvalidoException($"A linha {i + 1} possui um número de colunas inválido.");
                 }
@@ -29,18 +30,31 @@ internal class GerenciadorDeEstoque
                 {
                     throw new FormatoInvalidoException($"O nome do produto na linha {i + 1} está ausente.");
                 }
-                if (string.IsNullOrWhiteSpace(colunas[1]))
-                {
-                    throw new FormatoInvalidoException($"A categoria do preço na linha {i + 1} está ausente.");
-                }
                 if (!decimal.TryParse(colunas[1], NumberStyles.Any, CultureInfo.InvariantCulture, out decimal preco) || preco < 0)
                 {
                     throw new FormatoInvalidoException($"O preço do produto na linha {i + 1} é inválido ou negativo.");
+                }
+
+                string nome = colunas[0];
+
+                if (nome.ToLower().Contains("erro"))
+                {
+                    throw new FormatoInvalidoException($"O nome do produto na linha {i + 1} foi identificado como um erro ('{nome}').");
+                }
+                if (string.IsNullOrWhiteSpace(colunas[1]))
+                {
+                    throw new FormatoInvalidoException($"A categoria do preço na linha {i + 1} está ausente.");
                 }
                 if (string.IsNullOrWhiteSpace(colunas[2]))
                 {
                     throw new FormatoInvalidoException($"A categoria do produto na linha {i + 1} está ausente.");
                 }
+
+                string categoria = colunas[2];
+               
+                var produto = new Produto(nome, preco, categoria);
+
+                Produtos.Add(produto);
             }
             catch (FormatoInvalidoException ex)
             {
@@ -50,6 +64,25 @@ internal class GerenciadorDeEstoque
             {
                 Console.WriteLine($"Ocorreu um erro inesperado ao processar a linha {i + 1}: {ex.Message}");
             }
+        }
+    }
+
+    public void AdicionarProduto(Produto produto)
+    {
+        Produtos.Add(produto);
+        Console.WriteLine($"Produto {produto.Nome} adicionado com sucesso");
+    }
+
+    public void ListarProdutos()
+    {
+        if (Produtos.Count == 0)
+        {
+            Console.WriteLine("Nenhum produto cadastrado.");
+            return;
+        }
+        foreach (var produto in Produtos)
+        {
+            Console.WriteLine(produto);
         }
     }
 }
--- Leitr De Arquivo Com Relatorio/Program.cs
+++ Leitr De Arquivo Com Relatorio/Program.cs
@@ -1,7 +1,74 @@
-﻿class Program 
-{ 
-    static void Main(string[] args) 
-    { 
- 
+﻿using Leitr_De_Arquivo_Com_Relatorio;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var gerenciador = new GerenciadorDeEstoque();
+
+        ExibirOpçõesDoMenu();
+
+
+        void ExibirOpçõesDoMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n--- Menu Principal ---");
+                Console.WriteLine("1. Importar Produtos do CSV");
+                Console.WriteLine("2. Adicionar Produto");
+                Console.WriteLine("3. Remover Produto");
+                Console.WriteLine("4. Atualizar Produto");
+                Console.WriteLine("5. Listar Todos os Produtos");
+                Console.WriteLine("6. Consultar Produtos");
+                Console.WriteLine("7. Gerar Relatório");
+                Console.WriteLine("8. Sair");
+                Console.Write("Escolha uma opção: ");
+
+                var opcao = Console.ReadLine();
+
+                Console.Clear();
+
+                switch (opcao)
+                {
+                    case "1":
+                        gerenciador.ImportarArquivo("produtos_3.csv");
+                        Console.WriteLine("Importação concluída. Produtos válidos foram carregados.");
+                        break;
+                    case "2":
+                        AdicionarProdutoNovo();
+                        break;
+                    case "3":
+                        break;
+                        
+                    case "4":
+                        break;
+                        
+                    case "5":
+                        gerenciador.ListarProdutos();
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                }
+            }
+        }
+       
+        void AdicionarProdutoNovo()
+        {
+            Console.Write("Nome do produto: ");
+            var nome = Console.ReadLine();
+            Console.Write("Preço do produto (use , como separador): ");
+            var preco = decimal.Parse(Console.ReadLine());
+            Console.Write("Categoria do produto: ");
+            var categoria = Console.ReadLine();
+
+            var produto = new Produto(nome, preco, categoria);
+            gerenciador.AdicionarProduto(produto);
+        }
+
+
     }
 }
